f5176d72a2dc5524a1536b7673add51aa2fbf0e5
Johannes Kroll <jkroll@lavabit.com>
2011-04-06 17:01:52 +0200

    peak tracking

--- src/main.cpp
+++ src/main.cpp
@@ -10,6 +10,7 @@
 #include <sstream>
 #include <iostream>
 #include <fstream>
+#include <cmath>
 #include <GL/gl.h>
 #include <GL/glu.h>
 #include <SDL/SDL.h>
@@ -46,7 +47,7 @@ inline bool checkglerror(bool fatal= false)
 bool setVideoMode(int w, int h)
 {
 	SDL_Init(SDL_INIT_VIDEO);
-	SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 0);
+	SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
 	SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 0);
 	SDL_GL_SetAttribute(SDL_GL_SWAP_CONTROL, 0);
 	if(!SDL_SetVideoMode(w, h, 0, SDL_OPENGL|SDL_RESIZABLE))
@@ -297,7 +298,7 @@ bool configHandler::readFromFile(const char *filename)
 {
 	char *name, *itemName, *itemValue;
 	std::string lastName;
-	configOptionHandler *handler;
+	configOptionHandler *handler= 0;
 	char line[1024];
 
 	ifstream f;
@@ -439,6 +440,14 @@ class JackInterface
 			return (running= true);
 		}
 
+		void shutdown()
+		{
+		    if(!running) return;
+		    jack_deactivate(client);
+		    jack_client_close(client);
+		    running= false;
+		}
+
 		int getSamplingRate()
 		{
 			if(running)
@@ -538,6 +547,228 @@ class fluxWindowBase
 		virtual void cbMouse(primitive *self, int type, int x, int y, int btn) { }
 };
 
+
+
+class CFilterButterworth24db
+{
+public:
+    CFilterButterworth24db(void);
+    ~CFilterButterworth24db(void);
+    void SetSampleRate(float fs);
+    void Set(float cutoff, float q);
+    float Run(float input);
+
+private:
+    float t0, t1, t2, t3;
+    float coef0, coef1, coef2, coef3;
+    float history1, history2, history3, history4;
+    float gain;
+    float min_cutoff, max_cutoff;
+};
+
+// FilterButterworth24db.cpp
+
+#define BUDDA_Q_SCALE 1.f
+
+CFilterButterworth24db::CFilterButterworth24db(void)
+{
+    this->history1 = 0.f;
+    this->history2 = 0.f;
+    this->history3 = 0.f;
+    this->history4 = 0.f;
+
+    this->SetSampleRate(44100.f);
+    this->Set(22050.f, 0.0);
+}
+
+CFilterButterworth24db::~CFilterButterworth24db(void)
+{
+}
+
+void CFilterButterworth24db::SetSampleRate(float fs)
+{
+    float pi = 4.f * atanf(1.f);
+
+    this->t0 = 4.f * fs * fs;
+    this->t1 = 8.f * fs * fs;
+    this->t2 = 2.f * fs;
+    this->t3 = pi / fs;
+
+    this->min_cutoff = fs * 0.01f;
+    this->max_cutoff = fs * 0.45f;
+}
+
+void CFilterButterworth24db::Set(float cutoff, float q)
+{
+    if (cutoff < this->min_cutoff)
+        cutoff = this->min_cutoff;
+    else if(cutoff > this->max_cutoff)
+        cutoff = this->max_cutoff;
+
+    if(q < 0.f)
+        q = 0.f;
+    else if(q > 1.f)
+        q = 1.f;
+
+    float wp = this->t2 * tanf(this->t3 * cutoff);
+    float bd, bd_tmp, b1, b2;
+
+    q *= BUDDA_Q_SCALE;
+    q += 1.f;
+
+    b1 = (0.765367f / q) / wp;
+    b2 = 1.f / (wp * wp);
+
+    bd_tmp = this->t0 * b2 + 1.f;
+
+    bd = 1.f / (bd_tmp + this->t2 * b1);
+
+    this->gain = bd * 0.5f;
+
+    this->coef2 = (2.f - this->t1 * b2);
+
+    this->coef0 = this->coef2 * bd;
+    this->coef1 = (bd_tmp - this->t2 * b1) * bd;
+
+    b1 = (1.847759f / q) / wp;
+
+    bd = 1.f / (bd_tmp + this->t2 * b1);
+
+    this->gain *= bd;
+    this->coef2 *= bd;
+    this->coef3 = (bd_tmp - this->t2 * b1) * bd;
+}
+
+float CFilterButterworth24db::Run(float input)
+{
+    float output = input * this->gain;
+    float new_hist;
+
+    output -= this->history1 * this->coef0;
+    new_hist = output - this->history2 * this->coef1;
+
+    output = new_hist + this->history1 * 2.f;
+    output += this->history2;
+
+    this->history2 = this->history1;
+    this->history1 = new_hist;
+
+    output -= this->history3 * this->coef2;
+    new_hist = output - this->history4 * this->coef3;
+
+    output = new_hist + this->history3 * 2.f;
+    output += this->history4;
+
+    this->history4 = this->history3;
+    this->history3 = new_hist;
+
+    return output;
+}
+
+// something from http://www.musicdsp.org/showArchiveComment.php?ArchiveID=227
+class Butterworth
+{
+    public:
+        Butterworth():
+            State0(0), State1(0), State2(0), State3(0)
+        {
+            memset(this, 0, sizeof(*this));
+            setFrequency(5000, 44100);
+        }
+
+        void setFrequency(double Frequency, double Samplerate)
+        {
+            const double Q= 1.0;
+
+            // First calculate the prewarped digital frequency:
+            double K = tan(M_PI * Frequency / Samplerate);
+
+            // Now calc some intermediate variables: (see 'Factors of Polynoms' at http://en.wikipedia.org/wiki/Butterworth_filter, especially if you want a higher order like 48dB/Oct)
+            double a = 0.76536686473 * Q * K;
+            double b = 1.84775906502 * Q * K;
+
+            K = K*K;    // (to optimize it a little bit)
+
+            // Calculate the first biquad:
+
+            A0_I = 1.0/(K+a+1);
+            A1 = 2*(1-K);
+            A2 =(a-K-1);
+            B0 = K;
+            B1 = 2*B0;
+            B2 = B0;
+
+            // Calculate the second biquad:
+
+            A3_I = 1.0/(K+b+1);
+            A4 = 2*(1-K);
+            A5 = (b-K-1);
+            B3 = K;
+            B4 = 2*B3;
+            B5 = B3;
+        }
+
+        double run(double Input)
+        {
+            double Stage1 = B0*Input + State0;
+            State0 = B1*Input + A1*A0_I*Stage1 + State1;
+            State1 = B2*Input + A2*A0_I*Stage1;
+
+            double Output = B3*Stage1 + State2;
+            State2 = B4*Stage1 + A4*A3_I*Output + State3;
+            State3 = B5*Stage1 + A5*A3_I*Output;
+
+            return Output;
+        }
+
+    private:
+        double A0_I, A1, A2, A3_I, A4, A5,
+               B0, B1, B2, B3, B4, B5;
+        double State0, State1, State2, State3;
+};
+
+
+class PeakTracker
+{
+    public:
+        enum { MAXSAMPLES= 16 };
+
+        PeakTracker(): peak(0), peakFiltered(0), sampleIndex(0)
+        { memset(buf, 0, sizeof(buf)); setNumSamples(1); }
+
+        void setNumSamples(double n)
+        {
+            if(n>128) n= 128;
+            peakStep= 1.0/pow(n, 1.5);
+            peakStep2= 10*peakStep; if(peakStep2>1.0) peakStep2= 1.0;
+            n= (n>MAXSAMPLES-1? MAXSAMPLES-1: n<1? 1: n);
+            nSamples= n+0.5;
+        }
+
+        float run(float value)
+        {
+            buf[sampleIndex&(MAXSAMPLES-1)]= value;
+            peak= 0;
+            for(int i= nSamples-1; i>=0; i--)
+            {
+                float sample= buf[(sampleIndex+MAXSAMPLES-i) & (MAXSAMPLES-1)];
+                float sAbs= fabs(sample);
+                if(sAbs>peak)
+                    peak= sAbs;
+            }
+            sampleIndex++;
+            peakFiltered+= (peak-peakFiltered) * (peak>peakFiltered? peakStep2: peakStep);
+            return peakFiltered;
+        }
+
+    private:
+        float peak;
+        double peakFiltered;
+        float peakStep, peakStep2;
+        unsigned nSamples, sampleIndex;
+        float buf[MAXSAMPLES];
+};
+
 class fluxOscWindow: public fluxWindowBase, public configOptionHandler
 {
 	public:
@@ -596,13 +827,7 @@ class fluxOscWindow: public fluxWindowBase, public configOptionHandler
 		{ return verticalScaling; }
 
 		void setVerticalScaling(float s)
-		{
-			verticalScaling= (s<0.1? 0.1: s>100? 100: s);
-//			float triggerMinMax= 1.0/verticalScaling;
-//			if(triggerLevel>triggerMinMax) triggerLevel= triggerMinMax;
-//			if(triggerLevel<-triggerMinMax) triggerLevel= -triggerMinMax;
-//			updateGuiParam(&triggerLevel);
-		}
+		{ verticalScaling= (s<0.1? 0.1: s>100? 100: s); }
 
 		void setSamplingRate(float s)
 		{ samplingRate= s; }
@@ -618,9 +843,11 @@ class fluxOscWindow: public fluxWindowBase, public configOptionHandler
 
 		void addBuffers(jack_default_audio_sample_t **data, uint32_t nFrames, uint32_t nChannels)
 		{
-//            if(sampleBufferFillIndex>=sampleBuffers[0].size())
-//                sampleBufferFillIndex= 0;
             int srcPos= 0;
+            double samplesPerStep= double(sampleBuffers[0].size()) / glCoords[0].size();
+            for(unsigned i= 0; i<nChannels; i++)
+                filters[i].setNumSamples(samplesPerStep);
+            lineDisplayPeaks= (samplesPerStep>2.5);
 			if(triggerEnabled)
 			{
 				while(nFrames)
@@ -629,9 +856,21 @@ class fluxOscWindow: public fluxWindowBase, public configOptionHandler
 					int triggerPos= (sampleBufferFillIndex<sampleBuffers[0].size()? -1:
                                      findTriggerPos(data[0]+srcPos, nFrames-srcPos));
 					if(triggerPos>=0) blockSize= min(blockSize, triggerPos);
-					for(int i= 0; i<blockSize && sampleBufferFillIndex<sampleBuffers[0].size(); i++, sampleBufferFillIndex++)
-						for(unsigned ch= 0; ch<nChannels; ch++)
-							sampleBuffers[ch][sampleBufferFillIndex]= data[ch][srcPos+i];
+					unsigned idx;
+					for(unsigned ch= 0; ch<nChannels; ch++)
+                    {
+                        SampleVector &samples= sampleBuffers[ch];
+                        PeakTracker &filter= filters[ch];
+                        jack_default_audio_sample_t *chanData= data[ch];
+                        idx= sampleBufferFillIndex;
+                        if(lineDisplayPeaks)
+                            for(int i= 0; i<blockSize && idx<sampleBuffers[0].size(); i++, idx++)
+                                samples[idx]= filter.run(chanData[srcPos+i]);
+                        else
+                            for(int i= 0; i<blockSize && idx<sampleBuffers[0].size(); i++, idx++)
+                                samples[idx]= chanData[srcPos+i];
+                    }
+                    sampleBufferFillIndex= idx;
 					if(blockSize+srcPos<=triggerPos)
 						sampleBufferFillIndex= 0,
 						nFrames-= triggerPos;
@@ -645,9 +884,21 @@ class fluxOscWindow: public fluxWindowBase, public configOptionHandler
 				while(nFrames)
 				{
 					int blockSize= min(nFrames, sampleBuffers[0].size()-sampleBufferFillIndex);
-					for(int i= 0; i<blockSize; i++, sampleBufferFillIndex++)
-						for(unsigned ch= 0; ch<nChannels; ch++)
-							sampleBuffers[ch][sampleBufferFillIndex]= data[ch][srcPos+i];
+                    unsigned idx;
+                    for(unsigned ch= 0; ch<nChannels; ch++)
+                    {
+                        SampleVector &samples= sampleBuffers[ch];
+                        PeakTracker &filter= filters[ch];
+                        jack_default_audio_sample_t *chanData= data[ch];
+                        idx= sampleBufferFillIndex;
+                        if(lineDisplayPeaks)
+                            for(int i= 0; i<blockSize; i++, idx++)
+                                samples[idx]= filter.run(chanData[srcPos+i]);
+                        else
+                            for(int i= 0; i<blockSize; i++, idx++)
+                                samples[idx]= chanData[srcPos+i];
+                    }
+                    sampleBufferFillIndex= idx;
 					if(sampleBufferFillIndex>=sampleBuffers[0].size())
                         sampleBufferFillIndex= 0;
                     nFrames-= (blockSize? blockSize: nFrames);
@@ -661,8 +912,11 @@ class fluxOscWindow: public fluxWindowBase, public configOptionHandler
 		vector<SampleVector> sampleBuffers;
 		unsigned nChannels;
 		uint32_t sampleBufferFillIndex;
-		struct gl2DCoords { float x; float y; };
+		struct gl2DCoords { float x, y; float x1, y1; };
 		vector< vector<gl2DCoords> > glCoords;
+		struct gl3fColor { float r, g, b; float r1, g1, b1; };
+		vector< vector<gl3fColor> > glColors;
+		bool lineDisplayPeaks;
 		float triggerLevel;
 		bool triggerEnabled;
 		bool triggerPositive;
@@ -675,6 +929,7 @@ class fluxOscWindow: public fluxWindowBase, public configOptionHandler
 		int cursorPos;
 		unsigned cursorChannel;
 		class fluxOscWindowConfigPane *configPane;
+		PeakTracker filters[8];
 
 		int findTriggerPos(float *data, uint32_t nFrames)
 		{
@@ -694,17 +949,25 @@ class fluxOscWindow: public fluxWindowBase, public configOptionHandler
 
 		double getValueAtSamplePos(int channel, double pos)
 		{
-			uint32_t idx0= uint32_t(pos),
-				     idx1= (idx0<sampleBuffers[channel].size()-1? idx0+1: sampleBuffers[channel].size()-1);
-			double a= pos-idx0;
-			return sampleBuffers[channel][idx0]; // + (sampleBuffers[channel][idx1]-sampleBuffers[channel][idx0])*a;
+//			uint32_t idx0= uint32_t(pos),
+//				     idx1= (idx0<sampleBuffers[channel].size()-1? idx0+1: sampleBuffers[channel].size()-1);
+//			double a= pos-idx0;
+//			return sampleBuffers[channel][idx0] + (sampleBuffers[channel][idx1]-sampleBuffers[channel][idx0])*a;
+            return sampleBuffers[channel][pos];
 		}
 
 		double getValueAtCursorPos()
 		{
 		    if(cursorPos<0 || cursorChannel>sampleBuffers.size())
                 return 0;
-            return glCoords[cursorChannel][cursorPos].y;
+            if(triggerEnabled)
+                return glCoords[cursorChannel][cursorPos].y;
+            else
+            {
+                int offset= sampleBufferFillIndex*glCoords[cursorChannel].size()/sampleBuffers[0].size() + cursorPos;
+                offset%= glCoords[0].size();
+                return glCoords[cursorChannel][offset].y;
+            }
 		}
 
 		void updateGuiParam(void *paramAddress);
@@ -718,20 +981,41 @@ class fluxOscWindow: public fluxWindowBase, public configOptionHandler
 			if(!windowWidth) return;
 
 			if(glCoords.size()!=nChannels)
-                glCoords.resize(nChannels);
+                glCoords.resize(nChannels),
+                glColors.resize(nChannels);
 			for(unsigned i= 0; i<glCoords.size(); i++)
+            {
                 if(glCoords[i].size() != windowWidth)
                     glCoords[i].resize(windowWidth);
+                if(glColors[i].size() != windowWidth)
+                    glColors[i].resize(windowWidth);
+            }
 
 			double sampleStep= double(sampleBuffers[0].size())/glCoords[0].size();
+			float cr0= 0.1, cg0= 1.0, cb0= 0.2;
+			float cr1= 0.1, cg1= 1.0, cb1= .8;
+			float ct= 0.5;
 			for(unsigned i= 0; i<nChannels; i++)
             {
                 int coordIdx= 0;
+                vector<gl2DCoords> &coords= glCoords[i];
+                vector<gl3fColor> &colors= glColors[i];
                 for(double sampleIdx= 0; sampleIdx<(int)sampleBuffers[i].size() && coordIdx<(int)windowWidth;
                     sampleIdx+= sampleStep, coordIdx++)
                 {
-                    glCoords[i][coordIdx].x= coordIdx;
-                    glCoords[i][coordIdx].y= getValueAtSamplePos(i, sampleIdx);
+                    float value= getValueAtSamplePos(i, sampleIdx);
+                    coords[coordIdx].x= coordIdx;
+                    coords[coordIdx].y= value;
+                    coords[coordIdx].x1= coordIdx;
+                    coords[coordIdx].y1= 0;
+
+                    float c= fabs(value);
+                    float cr= (c-.75)*4;
+                    if(cr<0) cr= 0;
+                    else if(cr>.75) cr= .75;
+                    c= c*(1.0-ct)+ct;
+                    colors[coordIdx].r1= c*cr1; colors[coordIdx].g1= c*cg1; colors[coordIdx].b1= c*cb1;
+                    colors[coordIdx].r= ct*cr0+cr; colors[coordIdx].g= ct*cg0; colors[coordIdx].b= ct*cb0;
                 }
             }
 		}
@@ -801,36 +1085,68 @@ class fluxOscWindow: public fluxWindowBase, public configOptionHandler
         void paintSignalLines(int channel)
         {
             glColor4f(.1,1,.25,.75);
-            glEnable(GL_LINE_SMOOTH);
+            if(lineDisplayPeaks) glDisable(GL_LINE_SMOOTH);
+            else glEnable(GL_LINE_SMOOTH);
             glHint(GL_LINE_SMOOTH_HINT, GL_NICEST);
             glLineWidth(1.0);
             glEnable(GL_VERTEX_ARRAY);
 
+            if(lineDisplayPeaks)
+            {
+                glEnable(GL_COLOR_ARRAY);
+            }
+
+            int glCoordStride= (lineDisplayPeaks? 2*4: 4*4);
+
             if(triggerEnabled)
             {
                 // draw everything when trigger is on
-                glVertexPointer(2, GL_FLOAT, 0, (void*)&glCoords[channel][0]);
-                glDrawArrays(GL_LINE_STRIP, 0, glCoords[channel].size());
+                glVertexPointer(2, GL_FLOAT, glCoordStride, (void*)&glCoords[channel][0]);
+                if(lineDisplayPeaks)
+                {
+                    glColorPointer(3, GL_FLOAT, 0, &glColors[channel][0]);
+                    glDrawArrays(GL_LINES, 0, glCoords[channel].size()*2);
+                    glScalef(1, -1, 1);
+                    glDrawArrays(GL_LINES, 0, glCoords[channel].size()*2);
+                }
+                else glDrawArrays(GL_LINE_STRIP, 0, glCoords[channel].size());
             }
             else
             {
                 // trigger disabled: scroll
-                int endIndex= sampleBufferFillIndex*glCoords[channel].size()/sampleBuffers[0].size();
-                while(endIndex>glCoords[channel].size()) endIndex-= glCoords[channel].size();
+                int endIndex= sampleBufferFillIndex*glCoords[channel].size()/sampleBuffers[0].size()+1;
+                while(endIndex>(int)glCoords[channel].size()) endIndex-= glCoords[channel].size();
                 int right= glCoords[channel].size()-endIndex;
 
                 glTranslatef(right, 0, 0);
 
-                glVertexPointer(2, GL_FLOAT, 0, (void*)&glCoords[channel][0]);
-                glDrawArrays(GL_LINE_STRIP, 0, endIndex);
+                glVertexPointer(2, GL_FLOAT, glCoordStride, (void*)&glCoords[channel][0]);
+                if(lineDisplayPeaks)
+                {
+                    glColorPointer(3, GL_FLOAT, 0, &glColors[channel][0]);
+                    glDrawArrays(GL_LINES, 0, endIndex*2);
+                    glScalef(1, -1, 1);
+                    glDrawArrays(GL_LINES, 0, endIndex*2);
+                    glScalef(1, -1, 1);
+                }
+                else glDrawArrays(GL_LINE_STRIP, 0, endIndex);
 
                 glTranslatef(-right-endIndex, 0, 0);
 
-                glVertexPointer(2, GL_FLOAT, 0, (void*)&glCoords[channel][endIndex]);
-                glDrawArrays(GL_LINE_STRIP, 0, right);
+                glVertexPointer(2, GL_FLOAT, glCoordStride, (void*)&glCoords[channel][endIndex]);
+                if(lineDisplayPeaks)
+                {
+                    glColorPointer(3, GL_FLOAT, 0, &glColors[channel][endIndex]);
+                    glDrawArrays(GL_LINES, 0, right*2);
+                    glScalef(1, -1, 1);
+                    glDrawArrays(GL_LINES, 0, right*2);
+                    glScalef(1, -1, 1);
+                }
+                else glDrawArrays(GL_LINE_STRIP, 0, right);
             }
 
             glDisable(GL_VERTEX_ARRAY);
+            glDisable(GL_COLOR_ARRAY);
             glLineWidth(1);
         }
 
@@ -901,7 +1217,7 @@ class fluxOscWindow: public fluxWindowBase, public configOptionHandler
 				double windowPos= double(cursorPos)/windowWidth;
 				double valueAtCursor= getValueAtCursorPos();
 				double timeIdx= windowPos*sampleBuffers[cursorChannel].size()/samplingRate;
-				snprintf(cursorText, 128, "+%.2fms Value: %7.4f", timeIdx*1000, valueAtCursor);
+				snprintf(cursorText, 128, "+%.2fms Value: %7.4f %s", timeIdx*1000, valueAtCursor, lineDisplayPeaks? "(peak)": "");
 				draw_text(_font_getloc(FONT_DEFAULT), cursorText, 4,absPos->btm-4-13, *absPos, 0x10f008);
 			}
 
@@ -1413,6 +1729,7 @@ int main(int argc, char* argv[])
 
 		flux_tick();
 		glFinish();
+		SDL_GL_SwapBuffers();
 
 		double frametime= getTime() - time;
 		double delay= (1.0/100) - frametime;
@@ -1424,6 +1741,7 @@ int main(int argc, char* argv[])
 	if(!gConfigHandler.writeToFile(getConfigFilename().c_str()))
 		printf("couldn't write to config file %s\n", getConfigFilename().c_str());
 
+	JackIF.shutdown();
 	flux_shutdown();
 	SDL_Quit();
     return 0;
